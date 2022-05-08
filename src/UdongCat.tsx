accbdd48b1ba817406def449a1cdc284069f8e2b
J-young <beefiker@gmail.com>
2022-05-08 17:38:09 +0900

    Add tailwind-react-native-classnames

--- src/UdongCat.tsx
+++ src/UdongCat.tsx
@@ -1,22 +1,16 @@
 import TestComp from "components/TestComp";
 import { StyleSheet, Text, View } from "react-native";
+import tailwind from "twrnc";
 
 const UdongCat = () => {
   return (
-    <View style={styles.container}>
-      <Text>[Test] UdongCat</Text>
+    <View
+      style={tailwind`flex-col w-full h-full bg-black items-center justify-center`}
+    >
+      <Text style={tailwind`text-red-900 text-lg`}>[Test] UdongCat</Text>
       <TestComp />
     </View>
   );
 };
 
 export default UdongCat;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/components/TestComp.tsx
+++ src/components/TestComp.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Text } from "react-native";
+import tailwind from "twrnc";
 
 const TestComp = () => {
-  return <Text>TestComp</Text>;
+  return <Text style={tailwind`text-blue-800 text-4xl mt-10`}>TestComp</Text>;
 };
 
 export default TestComp;
