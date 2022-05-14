8de71f89c2cc136c5b4f6f820fb271ab59061027
J-young <beefiker@gmail.com>
2022-05-14 22:06:43 +0900

    Update Architecture

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
-const { plugin } = require("twrnc");
+import { plugin } from "twrnc";
 
-module.exports = {
-  plugins: [
-    plugin(({ addUtilities }) => {
-      addUtilities({
-        btn: {
-          padding: 3,
-          borderRadius: 10,
-          textTransform: `uppercase`,
-          backgroundColor: `#333`,
-        },
-        "resize-repeat": {
-          resizeMode: `repeat`,
-        },
-        "test-merong": `px-4 py-1 rounded-full bg-red-800 text-white`,
-        "flex-row-center": `flex flex-row justify-center items-center`,
-        "flex-col-center": `flex flex-col justify-center items-center`,
-      });
-    }),
-  ],
-};
+export const plugins = [
+  plugin(({ addUtilities }) => {
+    addUtilities({
+      btn: {
+        padding: 3,
+        borderRadius: 10,
+        textTransform: `uppercase`,
+        backgroundColor: `#333`,
+      },
+      "resize-repeat": {
+        resizeMode: `repeat`,
+      },
+      bgs: `bg-black dark:bg-white`,
+      "test-merong": `px-4 py-1 rounded-full bg-red-800 text-white`,
+      "flex-row-center": `flex flex-row justify-center items-center`,
+      "flex-col-center": `flex flex-col justify-center items-center`,
+    });
+  }),
+];
